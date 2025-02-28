3c79a6b7c432a64110c7cd2806f7cad84a0b561e
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-02-28 10:45:01 +0530

    [MANVI GROVER] ADD: Sam's average marks

---
+++ AverageMarks.java
@@ -0,0 +1,8 @@
+class AverageMarks{
+	public static void main(String[] args){
+		int maths=94;
+		int physics=95;
+		int chemistry=96;
+		System.out.println("Sam’s average mark in PCM is: "+(maths+physics+chemistry)/3);
+	}
+}
