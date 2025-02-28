c6cc85acd35b12cc08a5947809c639c7b74c290a
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-02-28 11:08:34 +0530

    [MANVI GROVER] ADD: README file

--- HarryAge.java
+++
@@ -1,7 +0,0 @@
-class HarryAge{
-	public static void main(String[] args){
-		int currentYear=2024;
-		int birthYear=2000;
-		System.out.println("Harry's age in 2024 is : "+(currentYear-birthYear));
-	}
-}
--- KilometersToMiles.java
+++
@@ -1,6 +0,0 @@
-class KilometersToMiles{
-	public static void main(String[] args){
-		double kilometers=10.8;
-		System.out.println("The distance "+kilometers+" km in miles is: "+ (kilometers*0.621371));
-	}
-}
