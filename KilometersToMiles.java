5ad8b34362abf4b94ec24b023efdc7714db5edcb
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-02-28 10:57:55 +0530

    [MANVI GROVER] ADD: Kilometers to miles distance

---
+++ KilometersToMiles.java
@@ -0,0 +1,6 @@
+class KilometersToMiles{
+	public static void main(String[] args){
+		double kilometers=10.8;
+		System.out.println("The distance "+kilometers+" km in miles is: "+ (kilometers*0.621371));
+	}
+}
