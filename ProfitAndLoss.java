5bb4059511bb8cc83e28d3120b475f09979d4708
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-02-28 23:11:38 +0530

    [MANVI GROVER] ADD: Calculated profit and profit percentage

---
+++ ProfitAndLoss.java
@@ -0,0 +1,9 @@
+class ProfitAndLoss{
+	public static void main(String[] args){
+		int cp=129;
+		int sp=191;
+		double profit=sp-cp;
+		System.out.println("The Cost Price is INR "+cp+" and Selling Price is INR "+sp+" The Profit is INR "+profit+" and the Profit Percentage is "+((profit/cp)*100)+" %");
+	}
+}
+
