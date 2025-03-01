e2356aa26d666b7ea23a72c7c121d4c859a66581
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-03-01 10:14:18 +0530

    [MANVI GROVER] ADD: Calculated distance in yards and miles

---
+++ DistanceInFeet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class DistanceInFeet{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		double feet=sc.nextDouble();
+		double distInYards=feet/3;
+		double distInMiles=distInYards/1760;
+		System.out.println("distance in yards and miles is "+distInYards+" and "+distInMiles);
+	}
+}
+
+		
